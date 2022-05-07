fd3c3e56b0d41969a64e3bc9fba605e84304934a
takahiro.tominaga <tt.300607@gmail.com>
2022-05-07 10:22:14 +0900

    feat: hello サーバーの作成 (#1)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kokoichi206/account-book-api
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.7
--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]string{"greeting": "hello world"})
+	})
+	r.Run(":11111")
 }
