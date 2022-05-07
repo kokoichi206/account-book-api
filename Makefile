c85d1f8973f7e0cacb5da50ec888befdbee79293
takahiro.tominaga <tt.300607@gmail.com>
2022-05-07 15:28:34 +0900

    feat: migration を実装 (#3)

--- Makefile
+++ Makefile
@@ -1,3 +1,20 @@
+DB_URL=postgresql://root:account@localhost:5432/account_book?sslmode=disable
+
+postgres:
+	docker run --name postgres12 --network account-network -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=account -d postgres:12-alpine
+
+createdb:
+	docker exec -it postgres12 createdb --username=root --owner=root account_book
+
+dropdb:
+	docker exec -it postgres12 dropdb account_book
+
+migrateup:
+	migrate -path db/migration -database "$(DB_URL)" -verbose up
+
+migratedown:
+	migrate -path db/migration -database "$(DB_URL)" -verbose down
+
 test:
 	go test -v -cover ./...
 
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,80 @@
+CREATE TABLE "users" (
+	"id" bigserial PRIMARY KEY,
+	"name" varchar,
+	"password" varchar NOT NULL,
+	"email" varchar UNIQUE NOT NULL,
+	"age" int NOT NULL,
+	"balance" bigint NOT NULL,
+	"password_changed_at" timestamptz NOT NULL DEFAULT '1970-01-01 00:00:00+00',
+	"created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "sessions" (
+	"id" uuid PRIMARY KEY,
+	"user_id" bigint NOT NULL,
+	"refresh_token" varchar NOT NULL,
+	"user_agent" varchar NOT NULL,
+	"client_ip" varchar NOT NULL,
+	"created_at" timestamptz NOT NULL DEFAULT (now()),
+	"expires_at" timestamptz NOT NULL
+);
+
+CREATE TABLE "expenses" (
+	"id" bigserial PRIMARY KEY,
+	"user_id" bigint NOT NULL,
+	"category_id" bigint NOT NULL,
+	"amount" bigint NOT NULL,
+	"food_receipt_id" bigint,
+	"comment" varchar,
+	"created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "categories" (
+	"id" bigserial PRIMARY KEY,
+	"name" varchar UNIQUE NOT NULL
+);
+
+CREATE TABLE "food_receipts" (
+	"id" bigserial PRIMARY KEY,
+	"store_name" varchar NOT NULL
+);
+
+CREATE TABLE "food_contents" (
+	"id" bigserial PRIMARY KEY,
+	"name" varchar NOT NULL,
+	"calories" float4 NOT NULL,
+	"lipid" float4 NOT NULL,
+	"carbohydrate" float4 NOT NULL,
+	"Protein" float4 NOT NULL
+);
+
+CREATE TABLE "food_receipt_contents" (
+	"id" bigserial PRIMARY KEY,
+	"food_receipt_id" bigint NOT NULL,
+	"food_content_id" bigint NOT NULL,
+	"amount" bigint NOT NULL
+);
+
+CREATE TABLE "transfers" (
+	"id" bigserial PRIMARY KEY,
+	"from_user_id" bigint NOT NULL,
+	"to_user_id" bigint NOT NULL,
+	"amount" bigint NOT NULL,
+	"created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+COMMENT ON COLUMN "expenses"."amount" IS 'can be negative or positive';
+COMMENT ON COLUMN "food_receipt_contents"."amount" IS 'must be positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "expenses" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "expenses" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+ALTER TABLE "expenses" ADD FOREIGN KEY ("food_receipt_id") REFERENCES "food_receipts" ("id");
+
+ALTER TABLE "food_receipt_contents" ADD FOREIGN KEY ("food_receipt_id") REFERENCES "food_receipts" ("id");
+ALTER TABLE "food_receipt_contents" ADD FOREIGN KEY ("food_content_id") REFERENCES "food_contents" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_user_id") REFERENCES "users" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_user_id") REFERENCES "users" ("id");
