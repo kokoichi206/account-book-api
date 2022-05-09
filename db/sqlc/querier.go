5e23347271f6ad2e6c8c9833e5d587abe69f7bac
takahiro.tominaga <tt.300607@gmail.com>
2022-05-09 21:13:20 +0900

    feat: レシート関連のdb操作を実装 (#5)

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -9,8 +9,14 @@ import (
 )
 
 type Querier interface {
+	CreateFoodContent(ctx context.Context, arg CreateFoodContentParams) (FoodContent, error)
+	CreateFoodReceipt(ctx context.Context, storeName string) (FoodReceipt, error)
+	CreateFoodReceiptContent(ctx context.Context, arg CreateFoodReceiptContentParams) (FoodReceiptContent, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
+	GetFoodContent(ctx context.Context, id int64) (FoodContent, error)
+	GetFoodReceipt(ctx context.Context, id int64) (FoodReceipt, error)
 	GetUser(ctx context.Context, email string) (User, error)
+	ListFoodReceiptContents(ctx context.Context, foodReceiptID int64) ([]ListFoodReceiptContentsRow, error)
 }
 
 var _ Querier = (*Queries)(nil)
