4c0d9beda1dd0029862bea607db50dd455adec6f
takahiro.tominaga <tt.300607@gmail.com>
2022-05-22 01:03:50 +0900

    test: セッションの自動更新の単体テストを追加 (#26)

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,18 +1,20 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"github.com/kokoichi206/account-book-api/auth"
-	db "github.com/kokoichi206/account-book-api/db/sqlc"
+	mockdb "github.com/kokoichi206/account-book-api/db/mock"
 	"github.com/kokoichi206/account-book-api/util"
 	"github.com/stretchr/testify/require"
 )
@@ -38,22 +40,33 @@ func TestAuthMiddleware(t *testing.T) {
 	testCases := []struct {
 		name          string
 		setupAuth     func(t *testing.T, request *http.Request, manager auth.SessionManager)
-		buildStubs    func(t *testing.T, manager auth.SessionManager)
-		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+		buildStubs    func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server)
 	}{
 		{
 			name: "OK",
 			setupAuth: func(t *testing.T, request *http.Request, manager auth.SessionManager) {
-				session := uuid.New()
+				session := manager.(*auth.MockUuidSessionManager).Uuid
 				addAuthorization(t, request, session.String())
 			},
-			buildStubs: func(t *testing.T, manager auth.SessionManager) {
+			buildStubs: func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager) {
+				querier.EXPECT().
+					UpdateSession(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(nil)
+
 				mockManager := manager.(*auth.MockUuidSessionManager)
+				mockManager.Uuid = uuid.New()
 				mockManager.Verify = true
 				mockManager.VerifyError = nil
 			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server) {
 				require.Equal(t, http.StatusOK, recorder.Code)
+
+				mockManager := server.sessionManager.(*auth.MockUuidSessionManager)
+				duration := server.config.SessionDuration.Seconds()
+				setCookieValue := fmt.Sprintf("session=%v; Path=/; Max-Age=%v; HttpOnly; Secure", mockManager.Uuid, duration)
+				require.Equal(t, setCookieValue, recorder.Header().Get("Set-Cookie"))
 			},
 		},
 		{
@@ -61,9 +74,9 @@ func TestAuthMiddleware(t *testing.T) {
 			setupAuth: func(t *testing.T, request *http.Request, manager auth.SessionManager) {
 				// Not setup cookie
 			},
-			buildStubs: func(t *testing.T, manager auth.SessionManager) {
+			buildStubs: func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager) {
 			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server) {
 				require.Equal(t, http.StatusUnauthorized, recorder.Code)
 				checkBodyContains(t, recorder, "cannot find cookie")
 			},
@@ -74,9 +87,9 @@ func TestAuthMiddleware(t *testing.T) {
 				// Cookie value is not uuid type.
 				addAuthorization(t, request, "wrong session type")
 			},
-			buildStubs: func(t *testing.T, manager auth.SessionManager) {
+			buildStubs: func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager) {
 			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server) {
 				require.Equal(t, http.StatusUnauthorized, recorder.Code)
 				checkBodyContains(t, recorder, "wrong cookie value found")
 			},
@@ -84,17 +97,41 @@ func TestAuthMiddleware(t *testing.T) {
 		{
 			name: "InvalidSession",
 			setupAuth: func(t *testing.T, request *http.Request, manager auth.SessionManager) {
-				session := uuid.New()
+				session := manager.(*auth.MockUuidSessionManager).Uuid
 				addAuthorization(t, request, session.String())
 			},
-			buildStubs: func(t *testing.T, manager auth.SessionManager) {
+			buildStubs: func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager) {
 				mockManager := manager.(*auth.MockUuidSessionManager)
+				mockManager.Uuid = uuid.New()
 				mockManager.Verify = false
 				mockManager.VerifyError = nil
 			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server) {
 				require.Equal(t, http.StatusUnauthorized, recorder.Code)
 				checkBodyContains(t, recorder, "session was not verified")
+			},
+		},
+		{
+			name: "DBErrorWhenUpdateSession",
+			setupAuth: func(t *testing.T, request *http.Request, manager auth.SessionManager) {
+				session := manager.(*auth.MockUuidSessionManager).Uuid
+				addAuthorization(t, request, session.String())
+			},
+			buildStubs: func(t *testing.T, querier *mockdb.MockQuerier, manager auth.SessionManager) {
+				querier.EXPECT().
+					UpdateSession(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(sql.ErrConnDone)
+
+				mockManager := manager.(*auth.MockUuidSessionManager)
+				mockManager.Uuid = uuid.New()
+				mockManager.Verify = true
+				mockManager.VerifyError = nil
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder, server *Server) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+
+				checkError(t, sql.ErrConnDone.Error(), recorder.Body)
 			},
 		},
 	}
@@ -107,22 +144,25 @@ func TestAuthMiddleware(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
-			querier := &db.Queries{}
+			config := util.Config{
+				SessionDuration: 10 * time.Minute,
+			}
+			querier := mockdb.NewMockQuerier(ctrl)
 			manager := auth.NewMockManager(querier)
-			server := NewServer(util.Config{}, querier, manager)
+			tc.buildStubs(t, querier, manager)
 
-			// managerについてはモックを使用する。
-			tc.buildStubs(t, manager)
+			server := NewServer(config, querier, manager)
 			// テスト用のパスを用意する。
 			authPath := "/auth"
 			server.router.GET(
 				authPath,
-				authMiddleware(server.sessionManager),
+				server.authMiddleware(server.sessionManager),
 				func(ctx *gin.Context) {
 					// authが通ったときはStatusOKを返す。
 					ctx.JSON(http.StatusOK, gin.H{})
 				},
 			)
+
 			recorder := httptest.NewRecorder()
 			request, err := http.NewRequest(http.MethodGet, authPath, nil)
 			require.NoError(t, err)
@@ -132,7 +172,7 @@ func TestAuthMiddleware(t *testing.T) {
 			server.router.ServeHTTP(recorder, request)
 
 			// Assert
-			tc.checkResponse(t, recorder)
+			tc.checkResponse(t, recorder, server)
 		})
 	}
 }
